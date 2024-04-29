3b9d827b09ff0198ecce2c953f882f39e62bd698
Ariel Barrios <ariel29111978@hotmail.com>
2024-04-29 10:49:45 -0300

    feat(App.tsx): add form

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,43 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+    <div>
+      <h1>Tutorial React Hook Form</h1>
+
+      <form className='card'>
+
+        <label htmlFor='name'>Name</label>
+        <input type='text' />
+
+        <label htmlFor='emain'>Email</label>
+        <input type='email' />
+
+        <label htmlFor='password'>Password</label>
+        <input type='password' />
+
+        <label htmlFor='confirmPassword'>Confirm password</label>
+        <input type='password' />
+
+        <label htmlFor='birddate'>Birddate</label>
+        <input type='date' />
+
+        <label htmlFor='country'>Country</label>
+        <select>
+          <option value='mx'>México</option>
+          <option value='co'>Colombia</option>
+          <option value='ar'>Argentina</option>
+        </select>
+
+        <label htmlFor='file'>Profile picture</label>
+        <input type='file' />
+
+        <label htmlFor='terms'>I accept terms and conditions</label>
+        <input type='checkbox' />
+
+        <button>Send</button>
+
+      </form>
+    </div>
   )
 }
 
